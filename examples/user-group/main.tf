21f9b52c3bac83bfae555ef3c0d3053e0ebfcd40
Phil Rodrigues <phil@hypr.co.nz>
2019-11-28 12:34:13 +1300

    Added output for the example terraform.

---
+++ examples/user-group/main.tf
@@ -0,0 +1,17 @@
+resource "aws_iam_group" "test_group" {
+  name = "test_group2"
+}
+
+module "example" {
+  create_user= true
+  create_iam_user_login_profile = true
+  source = "../../user-group"
+  name = "test_user2"
+  user_groups = ["${aws_iam_group.test_group.id}"]
+  pgp_key = "keybase:phiroict"
+  create_iam_access_key = true
+}
+
+output "pgp_message_key" {
+  value = "${module.example.keybase_password_pgp_message}"
+}
