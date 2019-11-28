5ac39d447b7e98281003ebe682a11a1e2f63d5ba
Phil Rodrigues <phil@hypr.co.nz>
2019-11-28 13:02:37 +1300

    Made example user-group identical to the one on 0.11

--- examples/user-group/main.tf
+++ examples/user-group/main.tf
@@ -1,17 +1,17 @@
 resource "aws_iam_group" "test_group" {
-  name = "test_group12"
+  name = "test_group2"
 }
 
 module "example" {
   create_user= true
   create_iam_user_login_profile = true
   source = "../../user-group"
-  name = "test_user12"
-  user_groups = [aws_iam_group.test_group.id]
+  name = "test_user2"
+  user_groups = ["${aws_iam_group.test_group.id}"]
   pgp_key = "keybase:phiroict"
   create_iam_access_key = true
 }
 
-output "pgp_secret_messages" {
-  value = module.example.keybase_password_pgp_message
+output "pgp_message_key" {
+  value = "${module.example.keybase_password_pgp_message}"
 }
