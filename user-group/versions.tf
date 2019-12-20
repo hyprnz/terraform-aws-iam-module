296589e7d3198bb8edda3aa5fc855c67e1188532
Phil Rodrigues <phil@hypr.co.nz>
2019-12-20 14:16:10 +1300

    Removed double provider definition.

--- user-group/versions.tf
+++ user-group/versions.tf
@@ -4,16 +4,3 @@ terraform {
     aws = "~> 2.19"
   }
 }
-
-provider "aws" {
-  alias  = "noassume"
-  region = var.aws_region
-}
-
-provider "aws" {
-  assume_role {
-    role_arn = "arn:aws:iam::${var.aws_account_number}:role/Administrator"
-  }
-  region = var.aws_region
-}
-
